ac555870eb35e0822f7be9f7a21a80ff2de7f699
brayanranda <brpenaranda@gmail.com>
2024-05-15 08:59:00 -0500

    Second commit

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,11 +1,26 @@
+import TaskForm from "./components/TaskForm";
 import TasksActions from "./components/TasksActions";
 import TasksList from "./components/TasksList";
+import { useTask } from "./hook/useTasks";
 
 function Tasks() {
+    const { task, tasks, handleChange, newTask, modal, showModal, removeTask } = useTask()
+
     return (
-        <main>
-            <TasksActions />
-            <TasksList />
+        <main className="relative">
+            <div className="relative z-0">
+                <TasksActions modal={modal} showModal={showModal} />
+                <TasksList tasks={tasks} removeTask={removeTask} />
+            </div>
+            {
+                modal &&
+                    <TaskForm
+                        task={task}
+                        newTask={newTask}
+                        showModal={showModal}
+                        handleChange={handleChange}
+                    />
+            }
         </main>
     );
 }
